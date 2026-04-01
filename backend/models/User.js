393b7c1514b55f36c7c4f065bdeeb79326bee05b
GREACY1 <greacykumari314@gmail.com>
2026-04-01 23:51:48 +0530

    Backend tested and working

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,22 +1,16 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name:     { type: String, required: true },
-  phone:    { type: String, required: true, unique: true },
-  role:     { type: String, enum: ['worker','customer'] },
-  skill:    { type: String },
-  photo:    { type: String },
-  location: {
-    type:        { type: String, default: 'Point' },
-    coordinates: [Number]
-  },
+  name:         { type: String, required: true },
+  phone:        { type: String, required: true, unique: true },
+  role:         { type: String, enum: ['worker','customer'] },
+  skill:        { type: String },
+  photo:        { type: String },
   fcmToken:     { type: String },
   rating:       { type: Number, default: 0 },
   totalRatings: { type: Number, default: 0 },
   isAvailable:  { type: Boolean, default: true },
   isSuspended:  { type: Boolean, default: false },
 }, { timestamps: true });
-
-UserSchema.index({ location: '2dsphere' });
 
 module.exports = mongoose.model('User', UserSchema);
